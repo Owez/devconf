73043f405c969fcd7dbe4cd43b1dcb6c213c0016
Owez <root@ogriffiths.com>
2020-07-07 01:31:55 +0100

    Set zsh as default shell

--- run_after.sh
+++ run_after.sh
@@ -1,2 +1,5 @@
 # Anything in here will be ran after everything else has been completed (run_before.sh -> apt packages -> run_mid.sh -> config files -> run_after.sh)
 # Should be used for final cleanups
+
+echo "Setting zsh as the default shell.."
+chsh -s $(which zsh)
