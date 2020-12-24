5515140376cd17e17cd777b78621752dc5d11cab
Owez <root@ogriffiths.com>
2020-12-24 00:24:53 +0000

    Fixed seclists check and better general dir checks

--- hack/install.sh
+++ hack/install.sh
@@ -14,17 +14,17 @@ fi
 
 cd $hackdir
 
-if [ ! -d "$hackdir/airgeddon" ]
+if [ ! -d airgeddon ]
 then
     git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git 
 fi
 
-if [ ! -d "$hackdir/dirsearch" ]
+if [ ! -d dirsearch ]
 then
     git clone https://github.com/maurosoria/dirsearch.git
 fi
 
-if [ ! -d "$hackdir/SecList" ]
+if [ ! -d SecLists-master ]
 then
     wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip
     unzip SecList.zip -o
