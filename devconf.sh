0bd40f7d47d6deaaa149e2991efbcf8e5e2ca26e
Owez <root@ogriffiths.com>
2021-04-14 23:04:22 +0100

    Basics finished

--- devconf.sh
+++ devconf.sh
@@ -1,10 +1,23 @@
 # autoinstaller
 
 # basic packages
-packages=("curl" "firefox" "code")
+packages=("python3" "vim" "curl" "firefox" "visual-studio-code-bin" "transmission-gtk" "spotify" "inkscape" "gimp" "discord")
 for package in ${packages[@]}; do
 	sudo pacman --noconfirm -S $package
 done
 
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# configure enviroment
+mkdir school
+cd school/
+mkdir documents
+mkdir code
+cd code/
+mkdir c
+mkdir html
+mkdir python
+mkdir rust
+mkdir shell
+mkdir other
