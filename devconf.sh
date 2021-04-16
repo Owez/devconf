87d02096d1396cd5acc6b75f70b67e1a8d1479ad
Owez <root@ogriffiths.com>
2021-04-16 23:23:19 +0100

    Code extensions & added audacity to package list

--- devconf.sh
+++ devconf.sh
@@ -1,13 +1,16 @@
 # autoinstaller
 
 # basic packages
-packages=("python3" "vim" "curl" "firefox" "visual-studio-code-bin" "transmission-gtk" "spotify" "inkscape" "gimp" "discord")
+packages=("python3" "vim" "curl" "firefox" "visual-studio-code-bin" "transmission-gtk" "spotify" "inkscape" "gimp" "discord" "audacity")
 for package in ${packages[@]}; do
 	sudo pacman --noconfirm -S $package
 done
 
-# ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install vscode extensions
+extensions=("adpyke.vscode-sql-formatter" "aeschli.vscode-css-formatter" "matklad.rust-analyzer" "ms-python.python" "ms-toolsai.jupyter" "PKief.material-icon-theme" "streetsidesoftware.code-spell-checker" "tomoki1207.pdf" "VisualStudioExptTeam.vscodeintellicode" "whizkydee.material-palenight-theme" "yzhang.markdown-all-in-one")
+for extension in ${extensions[@]}; do
+	code --install-extension $extension
+done
 
 # configure enviroment
 mkdir school
@@ -21,3 +24,6 @@ mkdir python
 mkdir rust
 mkdir shell
 mkdir other
+
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
