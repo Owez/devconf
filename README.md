cf8198ec2620ef0d3f4495c36119df4f6104ce1d
Owez <root@ogriffiths.com>
2021-04-17 19:05:29 +0100

    More to devconf

---
+++ README.md
@@ -0,0 +1,17 @@
+# devconf
+
+Installer for my dotfiles and general setup within a Manjaro i3 environment
+
+## Usage
+
+Use the script included in the root level of this repository like so:
+
+```bash
+# may be needed
+chmod +x ./devconf.sh 
+
+# run!
+./devconf.sh
+```
+
+And all configurations should be applied automatically!
--- devconf.sh
+++ devconf.sh
@@ -1,10 +1,26 @@
-# autoinstaller
+# Installer for my dotfiles and general setup within a Manjaro i3 environment
+
+# sort mirrors
+sudo pacman --noconfirm -S reflector
+reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+
+# update existing
+pacman --noconfirm -Syu
 
 # basic packages
-packages=("python3" "vim" "curl" "firefox" "visual-studio-code-bin" "transmission-gtk" "spotify" "inkscape" "gimp" "discord" "audacity")
+packages=("python" "vim" "curl" "wget" "firefox" "transmission-gtk" "spotify" "inkscape" "gimp" "discord" "audacity")
 for package in ${packages[@]}; do
 	sudo pacman --noconfirm -S $package
 done
+
+# install rust via rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install pulseaudio
+install_pulse
+
+# install vscode
+pamac build "visual-studio-code-bin" 
 
 # install vscode extensions
 extensions=("adpyke.vscode-sql-formatter" "aeschli.vscode-css-formatter" "matklad.rust-analyzer" "ms-python.python" "ms-toolsai.jupyter" "PKief.material-icon-theme" "streetsidesoftware.code-spell-checker" "tomoki1207.pdf" "VisualStudioExptTeam.vscodeintellicode" "whizkydee.material-palenight-theme" "yzhang.markdown-all-in-one")
@@ -12,18 +28,28 @@ for extension in ${extensions[@]}; do
 	code --install-extension $extension
 done
 
+# apply files
+cd files/
+cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+cp i3config $HOME/.i3/config
+
 # configure enviroment
 mkdir school
-cd school/
 mkdir documents
-mkdir code
-cd code/
+mkdir projects
+cd projects/
 mkdir c
 mkdir html
 mkdir python
 mkdir rust
 mkdir shell
 mkdir other
+cd ..
+
+# wallpaper
+wget https://cdn.osxdaily.com/wp-content/uploads/2018/06/macos-mojave-night-lightened-r.jpg -O sand-dunes.jpg
+sudo mv sand-dunes.jpg /usr/share/backgrounds/
+nitrogen /usr/share/backgrounds/sand-dunes.jpg
 
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
